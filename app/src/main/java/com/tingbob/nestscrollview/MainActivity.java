5eb6aba2c120cefdfdf546d82c43e925d472f8c5
nlv <tingbob@hotmail.com>
2017-01-02 19:19:16 +0800

    add fix footer view

--- app/src/main/java/com/tingbob/nestscrollview/MainActivity.java
+++ app/src/main/java/com/tingbob/nestscrollview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tingbob.nestscrollview;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
@@ -10,9 +11,14 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,10 +33,25 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
 
     private ViewPager mViewPager;
+
+    private View bottomView;
+    private View mPopupParentView = null; // 选择区域的view
+    private PopupWindow mPopupWindow = null;
+    private int screen_width = 0;
+    private int screen_height = 0;
+    private View mAnchorView;
+    private  int[] location = new int[2];
+    TranslateAnimation animation;// 出现的动画效果
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics metric = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metric);
+        screen_width = metric.widthPixels; // 屏幕宽度（像素）
+        screen_height = metric.heightPixels; // 屏幕高度（像素）
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         //使用系统navi back
@@ -84,6 +105,14 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("WebView"));
         tabLayout.addTab(tabLayout.newTab().setText("Other"));
         tabLayout.setupWithViewPager(mViewPager);
+
+        bottomView = findViewById(R.id.footer);
+        bottomView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPupupWindow();
+            }
+        });
     }
 
     private void setupViewPager(ViewPager mViewPager) {
@@ -121,5 +150,59 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitles.get(position);
         }
+    }
+
+    private void showPupupWindow() {
+        mPopupParentView = LayoutInflater.from(this).inflate(R.layout.bottom_pop, null);
+        initPopupWindow(mPopupParentView);
+
+        View blankView = mPopupParentView.findViewById(R.id.tv_bottom_pop);
+        blankView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mPopupWindow != null) {
+                    mPopupWindow.dismiss();
+                }
+            }
+        });
+        mPopupParentView.setAnimation(animation);
+        mPopupParentView.startAnimation(animation);
+        mPopupWindow.showAtLocation(mAnchorView, Gravity.NO_GRAVITY, location[0], location[1] - mPopupWindow.getHeight());
+    }
+
+    public void initPopupWindow(View view) {
+        /* 第一个参数弹出显示view 后两个是窗口大小 */
+        if (mPopupWindow == null) {
+            if (mAnchorView == null) {
+                mAnchorView = bottomView;
+            }
+            mAnchorView.getLocationOnScreen(location);// 获取控件在屏幕中的位置,方便展示Popupwindow
+            screen_height = location[1];
+            animation = new TranslateAnimation(0, 0, screen_height, 0);
+            animation.setDuration(500);
+            mPopupWindow = new PopupWindow(screen_width, screen_height);
+        }
+        mPopupWindow.setContentView(view);
+        /* 设置背景显示 */
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+		/* 设置触摸外面时消失 */
+        // mPopupWindow.setOutsideTouchable(true);
+
+        mPopupWindow.update();
+        mPopupWindow.setTouchable(true);
+		/* 设置点击menu以外其他地方以及返回键退出 */
+        mPopupWindow.setFocusable(true);
+
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+
+            }
+        });
+
+        /**
+         * 1.解决再次点击MENU键无反应问题 2.sub_view是PopupWindow的子View
+         */
+        view.setFocusableInTouchMode(true);
     }
 }
